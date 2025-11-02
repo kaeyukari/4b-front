46c4956c1e1ad8dba6cb93dbe7349c08ab5c55a9
kaeyukari <karenyukariyt19@gmail.com>
2025-11-02 19:57:40 +0000

    aula #2

--- src/app/index.js
+++ src/app/index.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Tela inicial</Text>
+      <Text>Tela inicial aa</Text>
       <StatusBar style="auto" />
     </View>
   );
